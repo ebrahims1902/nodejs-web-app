b5210ef87977eee790faf37a07e6800b049cf45e
ebrahims1902 <ebrahims1902@gmail.com>
2020-01-08 22:18:23 +0530

    weather app

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const  request = require('request')
+
 const geocode = require('./utlis/geocode')
-const forecast = require('./utlis/forecast')   
+const forecast = require('./utlis/forecast') 
 
-geocode('chennai',(error,data)=>{
-    console.log('Error',error)
-    console.log('Data',data)
+const address = process.argv[2]
+if(!address){
+    console.log('please provide an address')
+}else{
+
+    geocode(address,(error,data)=>{
+    if(error){
+      return  console.log(error)
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error){
+       return console.log(error)
+    }
+    console.log(data.location)
+    console.log(forecastData)
+    })
 })
-
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+}
