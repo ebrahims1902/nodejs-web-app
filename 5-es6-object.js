8945fccbdfa2adc2e05e4b147b496fb945d2e2ae
ebrahims1902 <ebrahims1902@gmail.com>
2020-01-13 18:24:03 +0530

    ES6 Aside: Object Property Shorthand and Destructuring

--- 5-es6-object.js
+++ 5-es6-object.js
@@ -7,11 +7,29 @@ const userAge = 18
 const user = {
     name,
     userAge,
-    location:'india,tamilnadu,chennai'
+    location:'chennai'
 }
 console.log(user)
 
+// object Destructuring
 
+const product = {
+    label : 'red notebook',
+    price : 3,
+    stock : 201,
+    saleprice : undefined,
+    rating : 4.2
+}
 
+// const label = product.label
+// const stock = product.stock
 
+// const {label :productlabel,stock ,rating = 5}= product
+// console.log(productlabel)
+// console.log(stock)
+// console.log(rating)
+const transaction = (type,{label,stock}) => {
+    console.log(type,label,stock)
 
+}
+transaction('order',product)
