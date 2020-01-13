3781a5f305a4e431075c25cf7fedd382e9a83392
ebrahims1902 <ebrahims1902@gmail.com>
2020-01-13 18:56:31 +0530

    ES6 Aside: Object Property Shorthand and Destructuring

--- utlis/forecast.js
+++ utlis/forecast.js
@@ -4,13 +4,13 @@ const  request = require('request')
 
 const forecast = (latitude, longitude, callback)=>{
     const url = 'https://api.darksky.net/forecast/' + latitude + ',' + longitude
-    request({ url: url,json: true}, (error,response)=>{
+    request({ url: url,json: true}, (error,{body})=>{
         if(error){
             callback('unable to connect to weather service. please try again.',undefined)
-        }else if (response.body.error){
+        }else if (body.error){
             callback('unable to find location',undefined)
         }else{
-            callback(undefined, console.log(response.body.daily.data[0].summary + 'it is currently ' + response.body.currently.temperature + ' farenheit. there is a '+ response.body.currently.precipProbability + ' % chance of rain.'))   
+            callback(undefined, console.log(body.daily.data[0].summary + 'it is currently ' + body.currently.temperature + ' farenheit. there is a '+ body.currently.precipProbability + ' % chance of rain.'))   
         }
     })
 }
--- utlis/geocode.js
+++ utlis/geocode.js
@@ -2,19 +2,19 @@ const request = require('request')
 
 const geocode = (address,callback) =>{
     const url =  'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token='
-    request({ url: url,json: true}, (error,response)=>{
+    request({url,json: true}, (error,{body})=>{
         if(error){
             console.log ('unable to connect web services!!!!!!')
         }
 
         
-        else if(response.body.features.length === 0){
+        else if(body.features.length === 0){
             callback('unable to find location. try another search',undefined)
         }else{
             callback(undefined,{
-                latitude:response.body.features[0].center[1],
-                longitude:response.body.features[0].center[0],
-                location:response.body.features[0].place_name
+                latitude:body.features[0].center[1],
+                longitude:body.features[0].center[0],
+                location:body.features[0].place_name
             })
         }
     })
