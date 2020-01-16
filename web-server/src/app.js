e40978ef6df5e8a83fc0358a22af4d1295b72d4c
ebrahims1902 <ebrahims1902@gmail.com>
2020-01-16 20:51:11 +0530

    web-app and creating web using handler

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,12 +5,28 @@ const express = require('express')
 const app = express()
 
 const publicDirectoryPath  = path.join(__dirname, '../public')
+app.set('view engine','hbs')
+
 app.use(express.static(publicDirectoryPath))
 
-
-app.get('/weather',(req,res)=>{
-    res.send('weather ...')
+app.get('',(req,res)=>{
+    res.render('index',{
+        title : 'weather app',
+        name : 'Ebrahim'
+    })
 })
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title : 'my photo',
+        name : 'Ebrahim'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helpText: 'This is an help message'
+    })
+})
+
 
 app.listen(8080,()=>{
     console.log('server up on port 8080')
