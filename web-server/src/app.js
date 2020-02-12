8c719124e639fe718b694dd5283d709a30c89e9b
ebrahims1902 <ebrahims1902@gmail.com>
2020-02-12 19:58:59 +0530

    web

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -58,26 +58,26 @@ app.get('/weather',(req, res)=>{
         }
 
         
-        // forecast(latitude, longitude, (error, forecastData)=>{
-        //     if(error){
-        //         return res.send({ error })
-        //     }
-        //     res.send({
-        //         forecast:forecastData,
-        //         location,
-        //         address:req.query.address
-        //     })
-        // })
-
-       let data =  await forecast(latitude, longitude)
-            
-     
-        console.log(">>>>>>>>>>>>",data)
+        forecast(latitude, longitude, (error, forecastData)=>{
+            if(error){
+                return res.send({ error })
+            }
             res.send({
-                forecast:data,
+                forecast:forecastData,
                 location,
                 address:req.query.address
             })
+        })
+
+    //    let data =  await forecast(latitude, longitude)
+            
+     
+    //     console.log(">>>>>>>>>>>>",data)
+    //         res.send({
+    //             forecast:data,
+    //             location,
+    //             address:req.query.address
+    //         })
 
 
     })
--- web-server/src/utlis/forecast.js
+++ web-server/src/utlis/forecast.js
@@ -1,33 +1,33 @@
 const  request = require('request')
 
-function forecast(latitude, longitude) {
-    return new Promise(async function(resolve,reject) {
-        const url = 'https://api.darksky.net/forecast/42fdc33db07a21cca70fb3416b97ac8b/' + latitude + ',' + longitude
-        request({url,json: true}, (error,{body})=>{
-            if(error){
-                reject('unable to connect to weather service. please try again.',undefined)
-            }else if (body.error){
-                reject('unable to find location',undefined)
-            }else{
-                return resolve(body.daily.data[0].summary + 'it is currently ' + body.currently.temperature + ' farenheit. there is a '+ body.currently.precipProbability + ' % chance of rain.')   
-            }
-        })
-    })
-}
-
-// const forecast = (latitude, longitude, callback)=>{
-//     const url = 'https://api.darksky.net/forecast/42fdc33db07a21cca70fb3416b97ac8b/' + latitude + ',' + longitude
-//     request({url,json: true}, (error,{body})=>{
-//         if(error){
-//             callback('unable to connect to weather service. please try again.',undefined)
-//         }else if (body.error){
-//             callback('unable to find location',undefined)
-//         }else{
-//             callback(console.log(body.daily.data[0].summary + 'it is currently ' + body.currently.temperature + ' farenheit. there is a '+ body.currently.precipProbability + ' % chance of rain.'))   
-//         }
+// function forecast(latitude, longitude) {
+//     return new Promise(async function(resolve,reject) {
+//         const url = 'https://api.darksky.net/forecast/42fdc33db07a21cca70fb3416b97ac8b/' + latitude + ',' + longitude
+//         request({url,json: true}, (error,{body})=>{
+//             if(error){
+//                 reject('unable to connect to weather service. please try again.',undefined)
+//             }else if (body.error){
+//                 reject('unable to find location',undefined)
+//             }else{
+//                 return resolve(body.daily.data[0].summary + 'it is currently ' + body.currently.temperature + ' farenheit. there is a '+ body.currently.precipProbability + ' % chance of rain.')   
+//             }
+//         })
 //     })
-    
 // }
+
+const forecast = (latitude, longitude, callback)=>{
+    const url = 'https://api.darksky.net/forecast/42fdc33db07a21cca70fb3416b97ac8b/' + latitude + ',' + longitude
+    request({url,json: true}, (error,{body})=>{
+        if(error){
+            callback('unable to connect to weather service. please try again.',undefined)
+        }else if (body.error){
+            callback('unable to find location',undefined)
+        }else{
+            callback(console.log(body.daily.data[0].summary + 'it is currently ' + body.currently.temperature + ' farenheit. there is a '+ body.currently.precipProbability + ' % chance of rain.'))   
+        }
+    })
+    
+}
 
 
 module.exports = forecast
